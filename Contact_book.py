726733d951d9bbf86cdfc94006da05b699d41da8
Precious Mapalo Nayingwa <Preciousnayingwa@gmail.com>
2023-08-18 15:22:39 +1100

    first commit

---
+++ Contact_book.py
@@ -0,0 +1,29 @@
+first_name = {'number':'name',
+           '1':'name1',
+            '2':'name 2',
+              '3':'name3',
+                '4':'name4',
+                 '5':'name 5'}
+
+address = {'number':'address',
+           '1':'address1',
+            '2':'address 2',
+              '3':'address3',
+                'r4':'address4',
+                 '5':'address4' }
+
+middle_name = {'number':'name',
+           'number1':'name1',
+            'number2':'name 2',
+              'number3':'name3',
+                'number4':'name4',
+                 'number5':'name 5'}
+
+surname = {'number':'name',
+           'number1':'name1',
+            'number2':'name 2',
+              'number3':'name3',
+                'number4':'name4',
+                 'number5':'name 5'}
+
+welcome=
